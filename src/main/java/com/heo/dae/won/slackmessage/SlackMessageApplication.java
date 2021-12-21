7785c14055ba0983cd8b45a895e5dc8cf79e5a3a
heo-dae-won <heodaewon92@kakao.com>
2021-12-21 13:30:08 +0900

    first commit

---
+++ src/main/java/com/heo/dae/won/slackmessage/SlackMessageApplication.java
@@ -0,0 +1,13 @@
+package com.heo.dae.won.slackmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heo/dae/won/slackmessage/SlackMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heo.dae.won.slackmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
